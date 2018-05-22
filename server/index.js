15869063e4854ddf96736681be72be48f9ad4e36
Madison Fierro <madisonmfierro@gmail.com>
2018-05-22 12:39:47 -0700

    Dunzo

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const session = require('express-session');
 const mc = require( `./controllers/messages_controller` );
 const app = express();
 require('dotenv').config;
+const createInitialSession = require('./middlewares/session');
+const filter = require('./middlewares/filter');
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
@@ -15,11 +17,20 @@ app.use(session({
         maxAge: 10000
     },
 }))
+app.use(createInitialSession);
+app.use((req, res, next) => {
+    if (req.method === 'POST' || req.method === 'PUT'){
+        filter (req, res, next)
+    } else {
+        next();
+    }
+});
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
 app.put( "/api/messages", mc.update );
 app.delete( "/api/messages", mc.delete );
+app.get("/api/messages/history", mc.history);
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
